f668a551e827ed3cee972bfb66dd04bf100a4fb7
petrosyansvetlana7@gmail.com <petrosyansvetlana7@gmail.com>
2019-12-12 19:03:15 +0400

    anketa partial

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
 
-     $('.customSelect .selectText>p').on('click' , function(){
-        $('.selectText').find('ul').removeClass('show')
+
+
+$('body').delegate('.customSelect .selectText>p', 'click' , function(){
+      $('.selectText').find('ul').removeClass('show')
         $('.selectText').removeClass('openItems')
         $(this).closest('.selectText').find('ul').toggleClass('show')
         $(this).closest('.selectText').toggleClass('openItems') 
+})
 
-     })
+  
+
+
 
 $('.customSelect ul li').on('click' , function() {
     $(this).closest('ul').find('li').removeClass('selected')
@@ -25,12 +30,47 @@ $('body').click(function(e) {
 });
 
 
+ new_elems_anketa = `
+            <div class="shownInfo">
+                        <div class="customSelect">
+                            <input  class="hidden" value="" type="text">
+                              <div class="selectText">
+                                  <p>Ներարկում </p>
+                                  <ul>
+                                      <li data-attr = 'Թռչուdfն'>Կատու</li>
+                                      <li data-attr = 'Թռչոssւն' class="selected">Թռչուն</li>
+                                      <li data-attr = 'Թռչուdfն'>Ձի</li>
+                                      <li data-attr = 'Թռdfrfչուն'>Գյուղատնտեսական կենդանի</li>
+                                  </ul>
+                              </div>
+                          </div>    
+                        <input type="text" placeholder="Ներարկման ամսաթիվը">
+                        <input type="text" placeholder="Նշումներ">
+                    </div>
+        `;
+$('.addSiring').on('click' , function() {
+    $(this).closest('.infoBySearch.anketa').find('.anketas').append(new_elems_anketa);
+})
+
+
+
+
+
 
 $('.closeModal').on('click' , function() {
     $('.popupForLogin').hide()
 })
 $('.logInButton').on('click' , function() {
     $('.popupForLogin').show()
+})
+
+ 
+$('.seeGuardian').on('click' , function() {
+    $('.popupForLogin.tesnelGtnveluvayr').show()
+})
+
+$('.seeLocation').on('click' , function() {
+    $('.popupForLogin.tesnelXnamoghin').show()
 })
 
 
